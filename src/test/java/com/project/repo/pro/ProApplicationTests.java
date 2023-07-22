33fc95f2506a50df4514bf7bb6c98a9ebceda2c6
albert-atabekyan <albert.atabekyan1200@gmail.com>
2023-07-22 08:10:56 +0300

    Временно удалил часть тестов

--- src/test/java/com/project/repo/pro/ProApplicationTests.java
+++ src/test/java/com/project/repo/pro/ProApplicationTests.java
@@ -1,31 +1,8 @@
 package com.project.repo.pro;
 
-import com.project.repo.pro.dao.UserDao;
-import com.project.repo.pro.model.User;
-import com.project.repo.pro.services.UserService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class ProApplicationTests {
-
-	@Autowired
-	UserDao userDao;
-
-	@Autowired
-	UserService userService;
-
-	@Test
-	@Transactional
-	void contextLoads() {
-		User user = userService.findUserById(6L);
-
-		String url = "kdjsklfj";
-		boolean flag = userService.addImage(user.getId(), url);
-		if(flag) System.out.println("Успех");
-		System.out.println("Error");
-	}
 
 }
