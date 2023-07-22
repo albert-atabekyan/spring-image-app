86a08e7bce397927006daed14ed6a8d196135d6d
albert-atabekyan <albert.atabekyan1200@gmail.com>
2023-07-22 19:03:13 +0300

    баг: слеш не нужен

--- src/main/java/com/atabekyan/image/service/config/MvcConfig.java
+++ src/main/java/com/atabekyan/image/service/config/MvcConfig.java
@@ -19,7 +19,7 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/**")
-                .addResourceLocations("file://" + uploadPath + "/");
+                .addResourceLocations("file:/" + uploadPath + "/");
     }
 
     @Override
