4aebf2e0e9ee2acd90d72e6ffa6c4b1d99ee546c
albert-atabekyan <albert.atabekyan1200@gmail.com>
2023-07-24 21:50:28 +0300

    Рефакторинг AuthenticationRequest

--- src/main/java/com/atabekyan/image/service/requests/AuthenticationRequest.java
+++ src/main/java/com/atabekyan/image/service/requests/AuthenticationRequest.java
@@ -1,21 +1,6 @@
 package com.atabekyan.image.service.requests;
 
-public class AuthenticationRequest {
-    private final String username;
-    private final String password;
-
-    public AuthenticationRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+public record AuthenticationRequest(String username, String password) {
 
     @Override
     public String toString() {
