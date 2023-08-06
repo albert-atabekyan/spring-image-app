10d37fbdb783582e13eca5023645a42dddd21dc5
albert-atabekyan <albert.atabekyan1200@gmail.com>
2023-08-06 20:30:30 +0300

    Импорт user details

--- src/main/java/com/atabekyan/image/service/controllers/ImageController.java
+++ src/main/java/com/atabekyan/image/service/controllers/ImageController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
