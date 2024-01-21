992a60494fa78774a2f2530064fbb623bf9b6778
albert-atabekyan <albert.atabekyan1200@gmail.com>
2024-01-21 19:06:41 +0300

    Теперь есть компонент, который отображает отдельную картинку

---
+++ src/main/frontend/src/Components/ImageItem.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import ImageService from "../Service/ImageService";
+import {useEffect, useState} from "react";
+import "../css/imageItem.css";
+import PopUp from "./PopUp";
+import Form from "./Form";
+
+const ImageItem = ({image_id, setImages}) => {
+    const [url, setUrl] = useState(undefined)
+
+    async function deleteImage(event) {
+        event.preventDefault();
+
+        const [image_id] = event.target.classList;
+
+        await ImageService.deleteImage(image_id);
+
+        const response = await ImageService.getImages();
+        const images = response.data;
+        setImages(images);
+    }
+
+    useEffect(() => {
+        ImageService.getImage(image_id).then(response => {
+            const blob = response.data;
+            const url = URL.createObjectURL(blob);
+            console.log(url)
+            setUrl(url)
+        });
+
+        ImageService.getImageInfo(image_id).then(response => {
+            const info = response.data;
+            const title = document.createElement("div")
+
+            title.textContent = info.title
+            title.classList.add("imageItemTitle");
+
+            const image_container = document.getElementById(image_id + "_div")
+            const secondElement = image_container.children[0];
+
+            image_container.insertBefore(title, secondElement.nextElementSibling);
+        });
+       }, []);
+
+
+    const input = [
+        {
+            id: 1,
+            name: "title",
+            type: "text",
+            placeholder: "Введите новый заголовок",
+            errorMessage:
+                "Пожалуйста, введите корректный заголовок.",
+            label: "Новый заголовок",
+            pattern: "^[A-Za-z0-9]{3,16}$",
+            required: true,
+        }
+    ];
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    };
+
+    return <div key={image_id} id={image_id + "_div"} className="image">
+            <img src={url} alt={image_id}></img>
+            <div className="buttonsContainer">
+                <button className={image_id + " imageItemButton"} onClick={(event => deleteImage(event))}>Удалить</button>
+                <PopUp 
+                    openText = "Редактировать"
+                    >
+                <Form 
+                    title="Редактировать заголовок"
+                    inputs={input}
+                    values={"title"}
+                    buttonTitle="Редактировать заголовок"
+                    >
+                </Form>
+                </PopUp>
+            </div>
+        </div>
+}
+
+export default ImageItem;
